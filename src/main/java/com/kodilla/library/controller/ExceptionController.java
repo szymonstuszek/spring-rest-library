74bd1a1053f6e26455302a399801a18293f36f53
szymonstuszek <szymonstuszek@gmail.com>
2019-03-17 07:48:08 +0100

    Added additional exception handling

--- src/main/java/com/kodilla/library/controller/ExceptionController.java
+++ src/main/java/com/kodilla/library/controller/ExceptionController.java
@@ -1,8 +1,6 @@
 package com.kodilla.library.controller;
 
-import com.kodilla.library.exception.ApiError;
-import com.kodilla.library.exception.BookCopyNotAvailableException;
-import com.kodilla.library.exception.RentalNotPossibleException;
+import com.kodilla.library.exception.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +14,7 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler({ RentalNotPossibleException.class })
-    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+    public ResponseEntity<Object> handleRentalNotPossibleException(
             RentalNotPossibleException ex, WebRequest request) {
         String error = "Cannot create rental - either account is disabled or book is not available.";
 
@@ -27,9 +25,42 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({ BookCopyNotAvailableException.class })
-    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+    public ResponseEntity<Object> handleBookCopyNotAvailableException(
             BookCopyNotAvailableException ex, WebRequest request) {
         String error = "Cannot create rental - book is not available.";
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), error);
+        return new ResponseEntity<>(
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ExceptionHandler({ BookCopyNotFoundException.class })
+    public ResponseEntity<Object> handleBookCopyNotFoundException(
+            BookCopyNotAvailableException ex, WebRequest request) {
+        String error = "Book copy not found.";
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), error);
+        return new ResponseEntity<>(
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ExceptionHandler({ UserNotFoundException.class })
+    public ResponseEntity<Object> handleUserNotFoundException(
+            BookCopyNotAvailableException ex, WebRequest request) {
+        String error = "User not found.";
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), error);
+        return new ResponseEntity<>(
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ExceptionHandler({ RentalNotFoundException.class })
+    public ResponseEntity<Object> handleRentalNotFoundException(
+            BookCopyNotAvailableException ex, WebRequest request) {
+        String error = "Rental not found.";
 
         ApiError apiError =
                 new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), error);
